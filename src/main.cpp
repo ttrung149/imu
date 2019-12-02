8c458137013f1d68b95f0dbabb4e605187d7ab6a
Trung Truong <ttruong91498@gmail.com>
2019-12-02 16:49:05 -0500

    Add I2C fast mode (400000 Hz)

--- src/main.cpp
+++ src/main.cpp
@@ -10,7 +10,7 @@ static uint32_t _micros;
 
 static float degrees_pitch_acc, degrees_roll_acc;
 static float acc_vector;
-static float degrees_pitch = 0, degrees_roll  = 0;
+static float degrees_pitch = 0, degrees_roll = 0;
 
 void read_MPU6050() {
     Wire.beginTransmission(MPU6050_ADDR);
@@ -49,21 +49,25 @@ void calibirate_MPU6050(int num_iterations) {
 
 void setup(){
     Wire.begin();
+    Wire.setClock(400000);
     Wire.beginTransmission(MPU6050_ADDR);
     Wire.write(0x6B);                      // PWR_MGMT_1 register (6B hex)
     Wire.write(0x00);                      // Activate the gyro.
     Wire.endTransmission(); 
+   
     Wire.beginTransmission(MPU6050_ADDR);
-    Wire.write(0x1B);                        // GYRO_CONFIG register (1B hex)
-    Wire.write(0x08);                        // Set 500dps full scale
+    Wire.write(0x1B);                      // GYRO_CONFIG register (1B hex)
+    Wire.write(0x08);                      // Set 500dps full scale
     Wire.endTransmission();
+   
     Wire.beginTransmission(MPU6050_ADDR);
-    Wire.write(0x1C);                        // ACCEL_CONFIG register (1A hex)
-    Wire.write(0x10);                        // (+/- 8g full scale range)
+    Wire.write(0x1C);                      // ACCEL_CONFIG register (1A hex)
+    Wire.write(0x10);                      // (+/- 8g full scale range)
     Wire.endTransmission();
+    
     Wire.beginTransmission(MPU6050_ADDR);
-    Wire.write(0x1A);                        // CONFIG register (1A hex)
-    Wire.write(0x03);                        // Set Digital LPF to ~43Hz
+    Wire.write(0x1A);                      // CONFIG register (1A hex)
+    Wire.write(0x03);                      // Set Digital LPF to ~43Hz
     Wire.endTransmission();   
 
     Serial.begin(115200);
@@ -80,6 +84,7 @@ void loop(){
 
     degrees_pitch += gyy * 0.0000610687;
     degrees_roll  += gyx * 0.0000610687;
+    // degrees_yaw   += gyz * 0.0000610687;
 
     degrees_pitch += degrees_roll * sin(gyz * 0.000001066);
     degrees_roll  -= degrees_pitch * sin(gyz * 0.000001066);
@@ -87,12 +92,16 @@ void loop(){
     acc_vector = sqrt((acx * acx) + (acy * acy) + (acz * acz));
     degrees_pitch_acc = asin((float) acy/acc_vector) * 57.2957795;
     degrees_roll_acc  = asin((float) acx/acc_vector) * -57.2957795;
+    
+    // degrees_yaw_acc = asin((float) acz/acc_vector) * 57.2957795;
 
-    degrees_pitch = degrees_pitch * 0.98 + degrees_pitch_acc * 0.02;
-    degrees_roll  = degrees_roll * 0.98 + degrees_roll_acc * 0.02;
+    degrees_pitch = degrees_pitch * 0.97 + degrees_pitch_acc * 0.03;
+    degrees_roll  = degrees_roll * 0.97 + degrees_roll_acc * 0.03;
+    // degrees_yaw  = degrees_yaw * 0.97 + degrees_yaw_acc * 0.03;
 
     Serial.print("Pitch: "); Serial.print(degrees_pitch);
     Serial.print(" | Roll: "); Serial.println(degrees_roll);
+    // Serial.print(" | Yaw: "); Serial.println(degrees_yaw);
     
     while(micros() < _micros + LOOP_PERIOD);
     _micros = micros();  
